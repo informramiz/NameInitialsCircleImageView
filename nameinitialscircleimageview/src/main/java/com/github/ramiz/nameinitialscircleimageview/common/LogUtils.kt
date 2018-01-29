23b025aa7ba121cdceeb1e66c207bee180311812
Ramiz Raja <informramiz@gmail.com>
2018-01-29 09:13:22 +0500

    feat: Create a custom logger class

---
+++ nameinitialscircleimageview/src/main/java/com/github/ramiz/nameinitialscircleimageview/common/LogUtils.kt
@@ -0,0 +1,36 @@
+package com.github.ramiz.nameinitialscircleimageview.common
+
+import android.util.Log
+import com.github.ramiz.nameinitialscircleimageview.BuildConfig
+
+/**
+ * Created by ramiz on 1/29/18.
+ * this class is Singleton
+ */
+
+object LogUtils {
+    fun v(tag: String, message: String) {
+        if (BuildConfig.DEBUG) return
+        Log.v(tag, message)
+    }
+
+    fun d(tag: String, message: String) {
+        if (BuildConfig.DEBUG) return
+        Log.d(tag, message)
+    }
+
+    fun i(tag: String, message: String) {
+        if (BuildConfig.DEBUG) return
+        Log.i(tag, message)
+    }
+
+    fun w(tag: String, message: String) {
+        if (BuildConfig.DEBUG) return
+        Log.w(tag, message)
+    }
+
+    fun e(tag: String, message: String) {
+        if (BuildConfig.DEBUG) return
+        Log.e(tag, message)
+    }
+}
